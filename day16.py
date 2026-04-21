1c206d367592c5957ab022d5104efc6b931d5629
unknown <ushnishmaharjan31@gmail.com>
2026-04-21 08:15:58 +0545

    fix

--- day16.py
+++ day16.py
@@ -1,27 +1,69 @@
-class modifier():
-    __a="this is private data"
-    b=__a
+class modifier:
+    __a = "this is private data"
+    b = __a
+
 
 class test(modifier):
-    d=2
+    d = 2
 
-obj=test()
+
+obj = test()
 print(obj.b)
 
 
-
-class accountinfo():
+class accountinfo:
 
     def setAccount(self, account_number, balance):
         self.__account_number = account_number
         self.__balance = balance
 
     def deposit(self, amount):
-        pass
+        self.__balance += amount
 
     def getBalance(self):
         balance = self.__balance
         return balance
 
     def displayAccount(self):
-        1
+        print("--- Account Info ---")
+        return f"""Account Number: {self.__account_number}
+Balance: {self.getBalance()}"""
+
+
+class savingsAccount(accountinfo):
+
+    def setSavingsDetails(self, interest_rate):
+        self.__InterestRate = interest_rate
+
+    def calculateInterest(self):
+        self.interest = self.getBalance() * self.__InterestRate / 100
+
+    def displaySavingsAccount(self):
+        return f"""{self.displayAccount()}
+--- Savings Info ---
+Interest: {self.interest}"""
+
+
+class PremiumSavings(savingsAccount):
+
+    def setBonous(self, bonus):
+        self.__bonus = bonus
+
+    def calculateTotalBalance(self):
+        self.total = self.getBalance() + self.interest + self.__bonus
+        return self.total
+
+    def displayPremiumDetails(self):
+        return f"""{self.displaySavingsAccount()}
+--- Premium Savings Info ---
+Bonous:{self.__bonus}
+Total Balance:{self.calculateTotalBalance()}"""
+
+
+obj = PremiumSavings()
+obj.setAccount(input("Enter Account Number: "), int(input("Enter Initial Balance:")))
+obj.deposit(1000)
+obj.setSavingsDetails(float(input("Enter Interest Rate: ")))
+obj.calculateInterest()
+obj.setBonous(float(input("Enter Bonus: ")))
+print(obj.displayPremiumDetails())
